a8d91292ec82fe61ef51b76ed624c119ee0b4209
Basit Ayuob <bayuob@ssidecisions.com>
2023-02-28 02:25:24 +0500

    <feat>: Added stayled compoents for ShopApp component

---
+++ src/pages/ShopApp/ShopApp.styles.ts
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+
+export const Header = styled.div`
+  background: #ffffff;
+`;
+
+export const HeaderImageWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 80px;
+`;
+
+export const HeaderImage = styled.img`
+  height: 60px
+`;
+
+export const MainImages = styled.span`
+  display: flex;
+  justify-content:
+  space-evenly; margin: 50px inherit;
+  padding: 32px 0;
+`;
+
+export const MainImage = styled.img`
+  max-height: 15rem;
+  display: block;
+`;
+
+export const MainBody = styled.div`
+  padding: 32px 0;
+  paddingTop: 0;
+`;
+
+export const ButtonWrapper = styled.div`
+  margin-bottom: 20px;
+`;
+
+export const MessageContainer = styled.div`
+  margin-bottom: 10px;
+  display: flex;
+  justify-content: center;
+`;
+
+export const StatsContainer = styled.div`
+  margin-bottom: 24px;
+`;
